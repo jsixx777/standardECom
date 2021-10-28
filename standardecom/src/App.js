5851b5638ec476d27a0d00a66d609e74c193ee42
JPSiler <jsixx77@hotmail.com>
2021-10-27 19:03:22 -0500

    add files

--- standardecom/src/App.js
+++ standardecom/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
+import './default.scss';
 
 export default function App () {
   return (
     <div className="main">
+      <Header />
       <h3 className="title">Yo React!</h3>
     </div>
   )
